c1f7f6bc9fc979161e89831da28817f620ea1808
Tolixx <tolixx@MacBook-Pro-John.local>
2013-02-12 16:53:36 +0200

    qcc

--- src/java/org/commoncrawl/examples/ExampleMetadataDomainPageCount.java
+++ src/java/org/commoncrawl/examples/ExampleMetadataDomainPageCount.java
@@ -288,7 +288,7 @@ public class ExampleMetadataDomainPageCount
 
     for (FileStatus fileStatus : fs.globStatus(new Path("/common-crawl/parse-output/valid_segments/[0-9]*"))) { 
       String[] parts = fileStatus.getPath().toString().split("/");
-      String inputPath = baseInputPath + "/" + parts[parts.length-1] + "/metadata-*";
+      inputPath = baseInputPath + "/" + parts[parts.length-1] + "/metadata-*";
       LOG.info("adding input path '" + inputPath + "'");
       FileInputFormat.addInputPath(job, new Path(inputPath));
       ++counter;
