a360735d5b22668a7ef3550376edacd5330f55a5
Tolixx <tolixx@MacBook-Pro-John.local>
2013-02-21 12:54:13 +0200

    import added

--- src/java/org/commoncrawl/examples/ExampleMetadataDomainPageCount.java
+++ src/java/org/commoncrawl/examples/ExampleMetadataDomainPageCount.java
@@ -6,6 +6,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 // Apache Project classes
