d92670b29bc2531d169649b98faa80dccbd70fe5
Tolixx <tolixx@MacBook-Pro-John.local>
2013-03-06 14:59:35 +0200

    metadata

--- src/java/org/commoncrawl/examples/ExampleBackwards.java
+++ src/java/org/commoncrawl/examples/ExampleBackwards.java
@@ -317,7 +317,7 @@ public class ExampleBackwards extends Configured implements Tool {
     }
     */
 
-    String inputPath = "s3n://aws-publicdatasets/common-crawl/parse-output/segment/1341690166822/textData-01666";
+    String inputPath = "s3n://aws-publicdatasets/common-crawl/parse-output/segment/1341690166822/metadata-01666";
     FileInputFormat.addInputPath(job, new Path(inputPath));
 
     LOG.info ( "We used : " + Integer.toString(used) + " segments, counter: " + Integer.toString(counter));
