702fbe2d312ad3c4271b0f5fd626f88840d52dd9
Tolixx <tolixx@MacBook-Pro-John.local>
2013-03-22 13:02:01 +0200

    cucomers

--- src/java/org/commoncrawl/examples/LinkParser.java
+++ src/java/org/commoncrawl/examples/LinkParser.java
@@ -331,7 +331,7 @@ public class LinkParser extends Configured implements Tool {
 
 
     if ( oneFile != null ) { 
-    	inputPath = "s3n://links_results/results/000001.tar.gz";
+    	inputPath = "s3n://linksresults/results/000006.tar.gz";
     	FileInputFormat.addInputPath(job, new Path(inputPath));
     	LOG.info ( "We just added inputPath : " + inputPath );
     }
